0abd206e0ef1e5f74694d315527573af8001191b
renanavisca-ops <renanavisca@gmail.com>
2026-08-26 15:59:24 -0600

    Mount dashboard launcher in upper-left

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import DashboardLauncher from './components/DashboardLauncher';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -10,6 +11,7 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
+    <DashboardLauncher />
     <App />
   </React.StrictMode>
 );
