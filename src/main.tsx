de2d8b05621663edf47935294332886030aa2b64
renanavisca-ops <renanavisca@gmail.com>
2026-08-26 16:01:44 -0600

    Revert mistaken dashboard mount

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import DashboardLauncher from './components/DashboardLauncher';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -11,7 +10,6 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <DashboardLauncher />
     <App />
   </React.StrictMode>
 );
